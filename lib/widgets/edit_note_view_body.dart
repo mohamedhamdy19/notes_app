6edd8e3576107d6d54a226e8f12181e9fdaafa9a
Mohamed Hamdy <mohamedhamdypnp@gmail.com>
2024-10-01 18:28:25 +0300

    note model created

--- lib/widgets/edit_note_view_body.dart
+++ lib/widgets/edit_note_view_body.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:notes_app/widgets/custom_add_button.dart';
 import 'package:notes_app/widgets/custom_app_bar.dart';
 import 'package:notes_app/widgets/custom_text_field.dart';
 
