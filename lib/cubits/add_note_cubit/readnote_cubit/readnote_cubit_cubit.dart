2c0bda16d771902d2c0c70d5efd7b6efa728adfc
Mohamed Hamdy <mohamedhamdypnp@gmail.com>
2024-10-12 20:43:11 +0300

    fetch all notes method created

--- lib/cubits/add_note_cubit/readnote_cubit/readnote_cubit_cubit.dart
+++ lib/cubits/add_note_cubit/readnote_cubit/readnote_cubit_cubit.dart
@@ -1,8 +1,20 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:notes_app/constants/constant.dart';
+import 'package:notes_app/models/note_model.dart';
 
 part 'readnote_cubit_state.dart';
 
-class ReadnoteCubit extends Cubit<ReadnoteState> {
-  ReadnoteCubit() : super(ReadnoteInitial());
+class ReadnoteCubit extends Cubit<ReadNoteState> {
+  ReadnoteCubit() : super(ReadNoteInitial());
+  fetchAllNotes() async {
+    try {
+      var noteBox = Hive.box<NoteModel>(knotesBox);
+      var notes = noteBox.values.toList();
+      emit(ReadNoteSuccess(notes));
+    } catch (e) {
+      emit(ReadnoteFail(errMessage: e.toString()));
+    }
+  }
 }
--- lib/cubits/add_note_cubit/readnote_cubit/readnote_cubit_state.dart
+++ lib/cubits/add_note_cubit/readnote_cubit/readnote_cubit_state.dart
@@ -1,15 +1,18 @@
 part of 'readnote_cubit_cubit.dart';
 
 @immutable
-sealed class ReadnoteState {}
+sealed class ReadNoteState {}
 
-final class ReadnoteInitial extends ReadnoteState {}
+final class ReadNoteInitial extends ReadNoteState {}
 
-final class ReadnoteLoading extends ReadnoteState {}
+final class ReadNoteLoading extends ReadNoteState {}
 
-final class ReadnoteSuccess extends ReadnoteState {}
+final class ReadNoteSuccess extends ReadNoteState {
+  final List<NoteModel> notes;
+  ReadNoteSuccess(this.notes);
+}
 
-final class ReadnoteFail extends ReadnoteState {
+final class ReadnoteFail extends ReadNoteState {
   final String errMessage;
 
   ReadnoteFail({required this.errMessage});
